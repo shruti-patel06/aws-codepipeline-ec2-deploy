57dc88bd22942c930c98fd3e31442cea7033deb2
Shruti <shruti.8717@gmail.com>
2026-02-20 19:22:26 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,10 @@
-echo "Hello World"
+#!/bin/bash
+# Copy new files from artifact directory to Apache web root
+cp -r /home/ssm-user/codepipeline-artifact/* /var/www/html/
+
+# Fix ownership and permissions
+chown -R apache:apache /var/www/html/
+chmod -R 755 /var/www/html/
+
+# Restart Apache to apply changes
+systemctl restart httpd
